c6858a93f352573186cc645d1236a59b899fc412
Sahariar Kabir <sronok@gmail.com>
2022-10-26 00:24:24 +0600

    added img

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 4000;
 const curriculums = require('./data/curriculum.json');
+const cateCurriculum = require('./data/cate.json');
 
 app.use(cors());
 
@@ -12,6 +13,15 @@ app.get('/' , (req ,res)=>{
 
 app.get('/curriculum', (req, res)=>{
     res.send(curriculums)
+});
+
+app.get('/cate', (req, res)=>{
+    res.send(cateCurriculum)
+});
+app.get('/cate/:id', (req, res)=>{
+    const id = req.params.id;
+    curriculumCateById = curriculums.filter( cur => cur.cate_id === id);
+    res.send(curriculumCateById)
 });
 
 app.get('/curriculum/:id' , (req , res) =>{
